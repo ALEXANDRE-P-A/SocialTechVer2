a3851c21bc4c5129b189976a4f60f4f15e8ee99c
ALEXANDRE PA <alexandrepereiraassano@gmail.com>
2022-08-30 13:21:06 +0900

    Section Contact added(only PC), Main-visual chaged

--- animation.js
+++ animation.js
@@ -1,3 +1,17 @@
 $(function(){
   $('body').css('background-color', 'rgb(39,16,213)');
+  $('.button-more').on('mouseover',function(){
+    $(this).animate({
+      opacity: 0.5,
+      marginLeft: 20,
+      // width: 300,
+    },100);
+  });
+  $('.button-more').on('mouseout',function(){
+    $(this).animate({
+      opacity: 1.0,
+      marginLeft: 0,
+      // width: 249,
+    },100);
+  });
 });
